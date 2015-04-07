7eda08878911ddcf4060352543daedf7a514a2db
calculator1 <alenyhka18@yandex.ru>
2015-04-07 14:41:41 +0300

    Update login.php

--- login.php
+++ login.php
@@ -2,21 +2,21 @@
 $db=mysql_connect('localhost', 'root', '');
 mysql_select_db('top10', $db);
  
-// Обезвреживаем переменные
+
 $blogin = htmlspecialchars($_POST[login]);
 $bpass = htmlspecialchars($_POST[pass]);
  
-//проверяем есть ли пользователь с таким login'ом и pass'ом
+
 $res=mysql_query("SELECT * FROM users WHERE login='$blogin' AND pass='$bpass'", $db);
-if(mysql_num_rows($res) != 1){    //такого пользователя нет
+if(mysql_num_rows($res) != 1){    
 echo "Введены не верные логин или пароль";
 }
-else{    //пользователь найден
+else{    
 session_start();
-$_SESSION['login']=$blogin;    //устанавливаем login & pass
+$_SESSION['login']=$blogin;    
 $_SESSION['pass']=$bpass;
 $_SESSION['var']=1;
-Header("Location: index.html");    // перенаправляем на index.html
+Header("Location: index.html");   
 mysql_close();
 }
 ?>
